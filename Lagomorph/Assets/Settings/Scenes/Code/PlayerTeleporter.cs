54e543abf448ce5be6dba2044d576d2059ea0969
Tyler <tylerdjohn04@gmail.com>
2025-04-07 18:36:33 -0500

    Code for the Teleporter/Door

---
+++ Lagomorph/Assets/Settings/Scenes/Code/PlayerTeleporter.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeleporter : MonoBehaviour
+{
+    private GameObject currentTeleporter;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (currentTeleporter != null)
+            {
+                transform.position = currentTeleporter.GetComponent<Door>().GetDestination().position;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Door"))
+        {
+            currentTeleporter = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Door"))
+        {
+            if (collision.gameObject == currentTeleporter)
+            {
+                currentTeleporter = null;
+            }
+        }
+    }
+}
